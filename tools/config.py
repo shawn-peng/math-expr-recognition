f54fa430792d75ab61fcaa02be6d096842a262d2
Yang <zhy0216@gmail.com>
2016-04-07 11:31:30 -0400

    change tool chain

---
+++ tools/config.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+IMAGE_TEST = "/home/yang/workspace/grad/zhang505-yisupeng-final/image/test1/"
+LISTFILE_TEST = "/home/yang/workspace/grad/zhang505-yisupeng-final/tools/listfile-test.txt"
+IMAGE_TEST_DATA_PATH = "/home/yang/workspace/grad/zhang505-yisupeng-final/data/test"
+
+IMAGE_TRAIN = "/home/yang/workspace/grad/zhang505-yisupeng-final/image/train1/"
+LISTFILE_TRAIN = "/home/yang/workspace/grad/zhang505-yisupeng-final/tools/listfile-train.txt"
+IMAGE_TRAIN_DATA_PATH = "/home/yang/workspace/grad/zhang505-yisupeng-final/data/train"
+
+CAFEE_ROOT = "/home/yang/workspace/caffe/"
+CONVERT_IMAGESET = CAFEE_ROOT + "build/tools/convert_imageset"
+
+
