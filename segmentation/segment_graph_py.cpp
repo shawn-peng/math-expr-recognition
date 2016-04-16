32fa4a24adc2cd332fd6e3f5c87003bdde1779de
yisupeng <yisupeng@iu.edu>
2016-04-16 01:19:49 -0400

    segment python interface

--- segmentation/segment_graph_py.cpp
+++ segmentation/segment_graph_py.cpp
@@ -1,22 +1,107 @@
+#include <iostream>
 
 #include <boost/python.hpp>
 
-#include "egbis/segment_graph.h"
+#include "egbis/image.h"
+#include "egbis/misc.h"
+#include "egbis/segment-graph.h"
+#include "egbis/segment-image.h"
 
-img * get_image(string filename)
+#include <string.h>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+
+using namespace cv;
+
+using namespace std;
+
+using namespace boost::python;
+
+void get_image(string filename)
 {
 
 }
 
-void* segment(const string &filename, float sigma, float c, int min_size)
+image<rgb>* convertMatToNativeImage(const cv::Mat& input)
 {
-   return ;
+	int w = input.cols;
+	int h = input.rows;
+	image<rgb> *im = new image<rgb>(w,h);
+
+	for(int i=0; i<h; i++)
+	{
+		for(int j=0; j<w; j++)
+		{
+			rgb curr;
+			cv::Vec3b intensity = input.at<cv::Vec3b>(i,j);
+			curr.b = intensity.val[0];
+			curr.g = intensity.val[1];
+			curr.r = intensity.val[2];
+			im->data[i*w+j] = curr;
+		}
+	}
+	return im;
 }
 
-BOOST_PYTHON_MODULE(get_image)
+
+dict segment(const string &filename, float sigma, float c, int min_size)
+{
+	int num_ccs;
+	Mat img = imread(filename, CV_LOAD_IMAGE_COLOR);
+
+	image<rgb> *natImage = convertMatToNativeImage(img);
+	universe *u = segmentation(natImage, sigma, c, min_size, &num_ccs);
+
+	dict forests;
+
+	//for (int i = 0; i < num_ccs; i++)
+	//{
+	//	forests.append(list());
+	//}
+	cout << len(forests) << endl;
+
+	for (int i = 0; i < img.rows; i++)
+	{
+		for (int j = 0; j < img.rows; j++)
+		{
+			int id = u->find(i*img.rows + j);
+			//list p(forests[id]);
+			//p.insert(0, make_tuple(i, j));
+			cout << id << endl;
+			//forests[id] = p;
+			if (!forests.has_key(id))
+			{
+				forests[id] = list();
+			}
+			list tmp;
+			tmp.append(make_tuple(i, j));
+			forests[id] += tmp;
+		}
+	}
+	return forests;
+}
+
+char const * print_str(const string &str)
+{
+	//printf(str.c_str());
+	return str.c_str();
+}
+
+BOOST_PYTHON_MODULE(segmentation)
 {
     using namespace boost::python;
-    def("get_image", get_image);
+    def("print_str", print_str);
+    def("segment", segment);
 }
+
+
+//BOOST_PYTHON_MODULE(print_str)
+//{
+//    using namespace boost::python;
+//    def("print_str", print_str);
+//}
 
 
