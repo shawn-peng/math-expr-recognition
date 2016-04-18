94638ba4ef1063640b9607fbbf03f28c87e4190b
Yang <zhy0216@gmail.com>
2016-04-17 23:30:22 -0400

    add model & update readme

---
+++ segmentation/model.py
@@ -0,0 +1,20 @@
+
+class ImageSymbol(object):
+    def __init__(self, pil_image, name="UNKNOW", 
+                                  left=None, right=None, 
+                                  top=None, bottom=None):
+        self.pil_image = pil_image
+        self.predict = None
+        self.name = name
+        self.left = left 
+        self.right = right
+        self.top = top
+        self.bottom = bottom
+        self.width = right - left
+        self.height = bottom - top
+        self.center = ((left+right)/2., (top+right)/2.)
+
+    def __str__(self):
+        return "%s: %s"%(self.name, self.predict)
+
+    
