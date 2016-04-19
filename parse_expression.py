18f2241f76ca21062ca1383ede4aa12d4dc0004d
Yang <zhy0216@gmail.com>
2016-04-19 19:24:17 -0400

    update

--- parse_expression.py
+++ parse_expression.py
@@ -2,11 +2,13 @@
    /2015/09/04/Deep-learning-tutorial-on-Caffe-Technology.html 
 '''
 
+import random
 import json
 import caffe ;caffe.set_mode_gpu();caffe.set_device(0);
 from PIL import Image, ImageOps
 import matplotlib.pyplot as plt
 import numpy as np ;np.set_printoptions(threshold=np.nan)
+from scipy import ndimage, misc
 
 from segmentation import extract
 from tools.config import *
@@ -34,21 +36,30 @@ def classify(pil_image, list_dict=LABEL_DICT):
     new_im.paste(im, ((new_size[0]-old_size[0])/2,
                       (new_size[1]-old_size[1])/2))
 
+    new_im = ndimage.grey_dilation(new_im, size=(2,2))
+    # print new_im
     # new_im.show()
+    # new_im = ndimage.binary_dilation(new_im, iterations=2)
+    # misc.imsave(str(random.randint(1,20))+"test.png", new_im)
+    # new_im.show()
+
+
     im = new_im
-    im = im.resize((28, 28), Image.ANTIALIAS)
+    im = misc.imresize(im, size=(28, 28))
+    # im = im.resize((28, 28), Image.ANTIALIAS)
+    # misc.imsave(str(random.randint(1,20))+"test.png", im)
     im = np.array(im)
 
     net.blobs['data'].data[...] = transformer.preprocess('data', im)
     #compute
     out = net.forward()
-    # print out
+
     index = most_common([x.argmax() for x in out['prob']])
     predict = None
     f = open(list_dict)
     data = json.load(f)
     data = dict((v,k) for k,v in data.iteritems())
-    print data
+    # print data
     predict = data[index]
     f.close()
     return predict
@@ -57,10 +68,10 @@ def fomula_decoder(image_path, list_dict=LABEL_DICT):
     image_symbol_list = extract(image_path)
     for img_sym in image_symbol_list:
         img_sym.predict = classify(img_sym.pil_image)
-        print img_sym.predict
+        # print img_sym.predict
     return image_symbol_list
 
 if __name__ == "__main__":
-    results = fomula_decoder("formula/gamma.png")
+    results = fomula_decoder("formula/a+b.png")
     for result in results:
         print result
--- tools/crawl.py
+++ tools/crawl.py
@@ -3,18 +3,18 @@ import time
 
 import requests
 from pyquery import PyQuery as pq
-folder = "./minus/"
+folder = "./backslash/"
 if not os.path.exists(folder):
     os.mkdir(folder)
 
-_id = 195
+_id = 949
 # trainingset
 tab = "trainingset"
-page = 9
+page = 1
 download_url = "http://www.martin-thoma.de/write-math"
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-i = 310
+i = 0
 while 1:
     url = "http://www.martin-thoma.de/write-math/symbol?id=%s&tab=%s&page=%s"%(_id, tab, page)
     d = pq(url=url)
